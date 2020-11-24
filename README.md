02fbf3fef90d576e1f08bde0fe8a06e04dc26c15
Sanderson <sanderson_imbeah5@hotmail.com>
2020-11-24 19:16:01 +0000

    adding in main project files

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a demo Appium project which comprises of the use of the Appium Service 
+which was pulled into the project via NPM. The project also comprises of WebdriverIO
+libaray used as a wrapper on top to interact with APIDemos-debug.apk.
+
+In order to run this project please ensure you have Android Studio installed and ensure 
+you have the correct Android SDK tools installed. Then create an Android virtual device
+via Android Studio and run the emulator.  
---
+++ test/specs/dialog.test.js
@@ -0,0 +1,30 @@
+const dialog = require('../../pageobjects/dialog.page').default;
+const expect = require ('chai').expect;
+
+describe('Dialog',()=>{
+  beforeEach(()=>{
+
+  });
+
+   it('Verify that the text entry dialog username & password fields are editable', ()=>{
+    dialog.appBtn.click();
+    dialog.alertDialogBtn.click();
+    dialog.textEntryDialogBtn.click();
+
+
+   dialog.userNameField.addValue("Test User");
+   dialog.userNameField.clearValue();
+   dialog.userNameField.addValue("Actual User");
+
+   dialog.passwordField.clearValue();
+   dialog.passwordField.addValue("Test Pass");
+    
+
+   let text = dialog.userNameField.getText();
+   console.log(text);
+    expect(text).equal("Actual User");
+
+    dialog.dialogOkBtn.click();
+});
+
+})
